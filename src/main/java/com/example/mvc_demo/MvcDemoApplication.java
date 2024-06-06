b3196b114d62b551af61e9fb9d081b66380386e8
Ezgi Çağla Sarıyıldız <e.sariyildiz2018@gtu.edu.tr>
2024-06-06 16:25:02 +0300

    06.06.2024

--- src/main/java/com/example/mvc_demo/MvcDemoApplication.java
+++ src/main/java/com/example/mvc_demo/MvcDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvcDemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MvcDemoApplication.class, args);
     }
 
--- src/main/java/com/example/mvc_demo/controller/HelloWorldController.java
+++ src/main/java/com/example/mvc_demo/controller/HelloWorldController.java
@@ -11,6 +11,7 @@ public class HelloWorldController {
     @RequestMapping(value = "/greeting")
     public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
         model.addAttribute("name", name);
+
         return "greeting";
     }
 
